411dcff4b06ef5cab98ea4fd007750362a1a7caa
administrator@WWD.LOCAL <administrator@WWD.LOCAL>
2020-02-12 09:19:08 -0800

    Add /api/user-groups

--- api/users.py
+++ api/users.py
@@ -19,7 +19,7 @@ def route_users():
 
     cmd = "Get-ADUser -filter {(enabled -eq $true) -and (emailaddress -ne 'null')} " +\
         "-properties DisplayName, EmailAddress | " +\
-        "select DisplayName, EmailAddress | ConvertTo-Json -Compress "
+        "select DisplayName, EmailAddress, SamAccountName | ConvertTo-Json -Compress "
 
     pscmd = 'powershell "' + cmd + '"'
 
@@ -28,4 +28,42 @@ def route_users():
     result = json.loads(output)
 
     return jsonify({'users': result}), 200
+
+
+@users.route('/user-groups', methods=['POST'])
+def route_user_groups():
+    valid_identity = False
+    try:
+        key = request.form['key']
+        expected = os.getenv('KEY')
+        if key != expected:
+            return jsonify({}), 204
+    except Exception as e:
+        return jsonify({}), 204
+        
+    try:
+        identity = request.form['identity']
+        if identity > '':
+            valid_identity = True
+        
+    except Exception as e:
+        return jsonify({}), 204
+
+    if not valid_identity:
+        return jsonify({}), 204 
+    
+    cmd = "Get-ADPrincipalGroupMembership -Identity " +\
+        identity + " | " +\
+        "select name | ConvertTo-Json -Compress "
+
+    pscmd = 'powershell "' + cmd + '"'
+
+    try:
+        proc = subprocess.Popen(pscmd, stdout=subprocess.PIPE)
+        output = proc.stdout.read().decode('ASCII')
+        result = json.loads(output)
+
+        return jsonify({'groups': result}), 200
+    except Exception as e:
+        return jsonify({'exception': 'Error obtaining user groups.' }), 500
 
