39e4a8945d1c7a521b308ad84ce3b4231b02eeec
jcarter62@gmail.com <jcarter62>
2020-02-13 16:56:29 -0800

    added one call that checks username and group membership

--- api/users.py
+++ api/users.py
@@ -67,3 +67,87 @@ def route_user_groups():
     except Exception as e:
         return jsonify({'exception': 'Error obtaining user groups.' }), 500
 
+
+@users.route('/userinfo', methods=['POST'])
+def route_user_info():
+    result = {
+        'authenticated': False,
+        'parceldocs': False,
+        'parceldocs-admin': False,
+        'user': {}
+    }
+
+    try:
+        key = request.form['key']
+        expected = os.getenv('KEY')
+        if key != expected:
+            return jsonify({}), 204
+
+        username = request.form['identity'].lower()
+    except Exception as e:
+        return jsonify({}), 204
+
+    cmd = "Get-ADUser -filter {(enabled -eq $true) -and (emailaddress -ne 'null')} " +\
+        "-properties DisplayName, EmailAddress | " +\
+        "select DisplayName, EmailAddress, SamAccountName | ConvertTo-Json -Compress "
+
+    pscmd = 'powershell "' + cmd + '"'
+
+    proc = subprocess.Popen(pscmd, stdout=subprocess.PIPE)
+    users = json.loads(proc.stdout.read().decode('ASCII'))
+    thisuser = None
+    for user in users:
+        if is_in_user_record(username, user):
+            thisuser = user
+            result['user'] = user
+            result['authenticated'] = True
+            break
+
+    if thisuser is None:
+        return jsonify({}), 204
+
+    identity = thisuser['SamAccountName']
+
+    cmd = "Get-ADPrincipalGroupMembership -Identity " +\
+        identity + " | " +\
+        "select name | ConvertTo-Json -Compress "
+
+    pscmd = 'powershell "' + cmd + '"'
+
+    try:
+        proc = subprocess.Popen(pscmd, stdout=subprocess.PIPE)
+        groups = json.loads(proc.stdout.read().decode('ASCII'))
+
+        isuser = False
+        isadmin = False
+        for group in groups:
+            if group['name'].lower() == 'parceldocs':
+                isuser = True
+            elif group['name'].lower() == 'parceldocs-admin':
+                isadmin = True
+
+        result['parceldocs'] = (isuser or isadmin)
+        result['parceldocs-admin'] = isadmin
+
+    except Exception as e:
+        return jsonify({'exception': 'Error obtaining user groups.'}), 500
+
+    return jsonify(result), 200
+
+
+def is_in_user_record(name, rec):
+    result = False
+    if not rec['DisplayName'] is None:
+        if rec['DisplayName'].lower() == name:
+            result = True
+
+    if not rec['EmailAddress'] is None:
+        if rec['EmailAddress'].lower() == name:
+            result = True
+
+    if not rec['SamAccountName'] is None:
+        if rec['SamAccountName'].lower() == name:
+            result = True
+
+    return result
+
